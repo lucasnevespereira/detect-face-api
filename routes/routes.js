ba38bfa800e1682a1b1a3da89c047e15b6ac4826
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-10-31 21:53:55 +0100

    added signin and register

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,54 @@
 const express = require("express");
 const router = express.Router();
 
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "Sally",
+      email: "sally@mail.com",
+      password: "test",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "Peter",
+      email: "peter@mail.com",
+      password: "test2",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
 router.get("/", (req, res) => {
-  res.send("This is still working!");
+  res.send(database.users);
+});
+
+router.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
+
+router.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+
+  res.json(database.users[database.users.length - 1]);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const routes = require("./routes/routes");
 
+app.use(bodyParser.json());
 app.use(routes);
 
 app.listen(3000, () => {
