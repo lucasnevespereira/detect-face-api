d7ddd62ec6a4c32d2a6577e90203fcaac0b11ee0
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-11-01 16:48:52 +0100

    improved register logic by adding a sql transaction

--- controllers/main.js
+++ controllers/main.js
@@ -48,23 +48,33 @@ exports.signin = (req, res) => {
 exports.register = (req, res) => {
   const { email, name, password } = req.body;
 
-  bcrypt.hash(password, 10, (err, hash) => {
-    if (!err) {
-      console.log(hash);
-    }
-  });
+  const hash = bcrypt.hashSync(password, 10);
 
-  db("users")
-    .returning("*")
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date(),
-    })
-    .then((user) => {
-      res.json(user[0]);
-    })
-    .catch((err) => res.status(400).json("unable to register"));
+  // transactions keep databases consistent to avoid failures
+  // all queries in a transaction run at the same time and if one fails they all fail
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            email: loginEmail,
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json("unable to register"));
 };
 
 exports.profile = (req, res) => {
