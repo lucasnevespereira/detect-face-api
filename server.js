796fead2bab71517b48419914cb0df16ad62334d
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-11-01 12:16:35 +0100

    added cors to server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const routes = require("./routes/routes");
+
+app.use(cors);
 
 app.use(bodyParser.json());
 app.use(routes);
