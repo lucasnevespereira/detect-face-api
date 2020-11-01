f3fdb869363a9f280f8fcdcb59a560ffc9581237
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-11-01 16:34:11 +0100

    increasing image entries count

--- controllers/main.js
+++ controllers/main.js
@@ -78,21 +78,20 @@ exports.profile = (req, res) => {
       }
     })
     .catch((err) => {
-      res.status(400).json("error getting user");
+      res.status(400).json("unable to get user");
     });
 };
 
 exports.image = (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(404).json("user not found");
-  }
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => {
+      res.status(400).json("unable to get entries");
+    });
 };
